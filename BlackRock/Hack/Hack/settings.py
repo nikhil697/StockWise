1e0010dadcd368fd148e669cd828d39ddf2965e6
Nikhil Chadha <58354293+nikhil697@users.noreply.github.com>
2024-08-07 02:28:23 +0530

    Update settings.py

--- BlackRock/Hack/Hack/settings.py
+++ BlackRock/Hack/Hack/settings.py
@@ -146,7 +146,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 #     BASE_DIR / "App\static",
 # ]
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, '/App/static'),
+    os.path.join(BASE_DIR, 'App/static'),
 ]
 
 import pymysql
