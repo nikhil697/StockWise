48e687bb2e606faddddceca2412a956de45b12a2
Nikhil Chadha <58354293+nikhil697@users.noreply.github.com>
2024-08-07 02:05:27 +0530

    Update settings.py

--- BlackRock/Hack/Hack/settings.py
+++ BlackRock/Hack/Hack/settings.py
@@ -134,7 +134,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
@@ -145,7 +145,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 #     BASE_DIR / "App\static",
 # ]
 STATICFILES_DIRS = [
-    BASE_DIR / "App" / "static",
+    os.path.join(BASE_DIR, 'App/static'),
 ]
 
 import pymysql
